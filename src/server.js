7861e9f7af2ed9216a0b92842c14149b608a7064
arjan-io <31731087+arjan-io@users.noreply.github.com>
2026-06-27 19:41:58 +0200

    Register roster data APIs

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ import { fileURLToPath } from "node:url";
 import importsRouter from "./routes/imports.router.js";
 import flightsRouter from "./routes/flights.router.js";
 import airportsRouter from "./routes/airports.router.js";
+import dutiesRouter from "./routes/duties.router.js";
+import paymentsRouter from "./routes/payments.router.js";
+import issuesRouter from "./routes/issues.router.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,6 +21,9 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use("/api/imports", importsRouter);
 app.use("/api/flights", flightsRouter);
 app.use("/api/airports", airportsRouter);
+app.use("/api/duties", dutiesRouter);
+app.use("/api/payments", paymentsRouter);
+app.use("/api/issues", issuesRouter);
 
 app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
