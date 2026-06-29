0fb4da643cf1d15e262842c55e34e4e186e490f9
arjan-io <31731087+arjan-io@users.noreply.github.com>
2026-06-29 12:08:05 +0200

    Expose duty type CRUD APIs

--- src/routes/duties.router.js
+++ src/routes/duties.router.js
@@ -1,15 +1,26 @@
 import express from "express";
 import {
+  deleteDutyType,
   deleteMiscDuty,
   listDutyTypes,
   listMiscDuties,
   markMiscDutyPaid,
+  saveDutyType,
   saveMiscDuty
 } from "../services/duty.service.js";
 
 const router = express.Router();
 
 router.get("/types", (_req, res) => res.json(listDutyTypes()));
+router.post("/types", (req, res, next) => {
+  try { res.json(saveDutyType(req.body)); } catch (error) { next(error); }
+});
+router.put("/types/:id", (req, res, next) => {
+  try { res.json(saveDutyType({ ...req.body, id: req.params.id })); } catch (error) { next(error); }
+});
+router.delete("/types/:id", (req, res, next) => {
+  try { res.json(deleteDutyType(req.params.id)); } catch (error) { next(error); }
+});
 router.get("/", (_req, res) => res.json(listMiscDuties()));
 
 router.post("/", (req, res, next) => {
