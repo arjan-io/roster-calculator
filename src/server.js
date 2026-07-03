a6ac7aa806bd0b417ce95916a478fcbcbe4b9d46
arjan-io <31731087+arjan-io@users.noreply.github.com>
2026-07-03 15:43:17 +0200

    Repair missing flight distances on startup

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import airportsRouter from "./routes/airports.router.js";
 import dutiesRouter from "./routes/duties.router.js";
 import paymentsRouter from "./routes/payments.router.js";
 import issuesRouter from "./routes/issues.router.js";
+import { recalculateFlightDistances } from "./services/airport.service.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -28,6 +29,8 @@ app.use("/api/issues", issuesRouter);
 app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+recalculateFlightDistances({ onlyMissing: true });
 
 app.listen(port, () => {
   console.log(`Roster Calculator running at http://localhost:${port}`);
